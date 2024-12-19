09950fe908d553a64be08c62fef95dde1425b440
bradyDarren <dcgbfinancial@gmail.com>
2024-12-19 08:31:34 -0600

    addition of the output list into source code

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import colorgram
+# import colorgram
 
-colors = colorgram.extract("painting.jpg",6)
+# colors = colorgram.extract("painting.jpg",30)
 
-rgb_values = []
+# rgb_values = []
 
-for rgb in range(len(colors)):
-    r = colors[rgb].rgb.r
-    g = colors[rgb].rgb.g
-    b = colors[rgb].rgb.b
-    color_group = (r,g,b)
-    rgb_values.append(color_group)
-print(rgb_values)
+# for rgb in range(len(colors)):
+#     r = colors[rgb].rgb.r
+#     g = colors[rgb].rgb.g
+#     b = colors[rgb].rgb.b
+#     color_group = (r,g,b)
+#     rgb_values.append(color_group)
+# print(rgb_values)
+
+color_list = [(198, 13, 32), (248, 236, 25), (40, 76, 188), (39, 216, 69), 
+(238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), 
+(61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), 
+(73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51)]
